4b2aa6b8be3c3a8d3e5a14dbe617edd88fe3f047
Kevin Bandison <kbandison@gmail.com>
2022-09-08 12:56:55 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('HH   HH          LLL  LLL            WWW           WWW                LL  LL       DD  !!!');
+console.log('HH   HH    EEE   LLL  LLL   OOOO      WWW         WWW   OOO  RR  RR   LL  LL       DD  !!!');
+console.log('HHHHHHH EE   E   LLL  LLL  OO  OO       WWW  WWW WWW  OO  OO RR    R  LL  LL   DDDDDD  !!!');
+console.log('HH   HH EEEEE    LLL  LLL  OO  OO       WWW      WWW  OO  OO RR       LL  LL  DD   DD     ');
+console.log('HH   HH   EEEEE  LLL  LLL   OOOO          WWW  WWW     OOOO  RR       LL  LL   DDDDDD  !!!');
